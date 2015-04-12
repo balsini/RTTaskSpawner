7f6782f15f88bf2f74ae792d7fdfc8c6d47e1589
Alessio Balsini <a.balsini@sssup.it>
2015-04-12 15:33:45 +0200

    Updated indentation and wrong name of 'ss'

--- src/periodicity.h
+++ src/periodicity.h
@@ -15,31 +15,31 @@
  * All the values are in ms
  */
 typedef struct periodic_task_attr_ {
-	void * arg;			// task argument
-    int dmiss;			// number of deadline misses
+  void * arg;		// task argument
+  int dmiss;		// number of deadline misses
 
-    // The task acts as follows:
-    //
-    //    c0     dd   c1    s_runtime    deadline
-    // |#######------####_______)___________| . . .
+  // The task acts as follows:
+  //
+  //    c0     ss   c1    s_runtime    deadline
+  // |#######======####_______)___________| . . .
 
-    int jobs;           // Number of jobs for each task
-    int ss_every;       // The task Self-Suspends after every ss_every
-                        //   activations
-    int ss;             // The task Self-Suspends after ss_every activations
-    int c0;             // Duration of C0
-    int c1;             // Duration of C1
+  int jobs;		// Number of jobs for each task
+  int ss_every;		// The task Self-Suspends after every ss_every
+			//   activations
+  int ss;		// The task Self-Suspends after ss_every activations
+  int c0;		// Duration of C0
+  int c1;		// Duration of C1
 
-    int period;			// task period
-    int deadline;		// task deadline
+  int period;		// task period
+  int deadline;		// task deadline
 
-    int s_period;       // server period
-    int s_deadline;     // server deadline
-    int s_runtime;      // server runtime that will be used
+  int s_period;		// server period
+  int s_deadline;	// server deadline
+  int s_runtime;	// server runtime that will be used
                         //   by CBS for bandwidth management
 
-	struct timespec at;	// next activation time
-    struct timespec dl;	// abs. deadline
+  struct timespec at;	// next activation time
+  struct timespec dl;	// abs. deadline
 } periodic_task_attr;
 
 void periodicity_init();
