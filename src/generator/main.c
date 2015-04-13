f76c09c44f6d014e8b6aad4817a0c1d999a0575f
Alessio Balsini <a.balsini@sssup.it>
2015-04-13 18:06:43 +0200

    Added thread number parameter passing

--- src/generator/main.c
+++ src/generator/main.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
 #include <sys/types.h>
 
 #include "../periodicity.h"
 #include "random_task_generator.h"
 
-int main()
+int main(int argc, char *argv[])
 {
   periodic_task_attr *p;
-  unsigned int size = 50;
+  unsigned int size = 10;
+  int c;
+  char *tvalue = NULL;
+
+
+  opterr = 0;
+  while ((c = getopt (argc, argv, "t:")) != -1) {
+    switch (c)
+    {
+      case 't':
+        tvalue = optarg;
+        size = atoi(tvalue);
+        break;
+      case '?':
+        if (optopt == 't')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+    }
+  }
 
   random_task_generator(&p,
                         size,
